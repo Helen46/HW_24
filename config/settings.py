38134ac3b9e4eb52db800bce371760c38ef3d065
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-10-30 12:31:20 +0300

    add filter and ordering for Payment

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters'
 
     'users',
     'lms',
@@ -78,6 +79,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 
 # Database
--- users/views.py
+++ users/views.py
@@ -1,14 +1,23 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.generics import CreateAPIView
 from rest_framework.viewsets import ModelViewSet
 
-from users.models import User
+from users.models import User, Payment
 from users.serializer import UserSerializer, PaymentSerializer
+
+
+class PaymentViewSet(ModelViewSet):
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_type',)
+    ordering_fields = ('payment_type',)
+
+
+class PaymentCreateAPIView(CreateAPIView):
+    serializer_class = PaymentSerializer
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class PaymentCreateAPIView(CreateAPIView):
-    serializer_class = PaymentSerializer
